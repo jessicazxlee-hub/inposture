5643cd3b716baf1e63c24681f9e59478968c4ae4
Jessica Lee <234859400+jessicazxlee-hub@users.noreply.github.com>
2026-03-16 17:49:36 -0400

    Convert ios-app from submodule to normal directory

---
+++ ios-app/inposture2/ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ContentView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        ViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+}
---
+++ ios-app/inposture2/inposture2App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct inposture2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
