68717cd3137cb3b1effdd5e29121cd989db7884d
Jessica Lee <234859400+jessicazxlee-hub@users.noreply.github.com>
2026-03-16 14:08:55 -0400

    move iOS app into ios-app directory

--- inposture2/ContentView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct ContentView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> ViewController {
-        ViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
-}
